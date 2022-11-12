c868380382c863ee08c27d9b151eb1934895d7b4
GanerCodes <ganer@ganer.xyz>
2022-11-12 16:30:21 -0600

    xd

---
+++ website/API.js
@@ -0,0 +1,334 @@
+// const host = window.location.protocol + "//" + window.location.host + "/";
+const host = "http://127.0.0.1:5000/";
+
+var user_name = null;
+var user_pass = null;
+
+window.onload = () => {
+    user_name = localStorage.getItem("user_name");
+    user_pass = localStorage.getItem("user_pass");
+    if(user_name && user_pass) login(user_name, user_pass)
+}
+function isLoggedIn() {
+    return (user_name && user_pass);
+}
+function login(user_name, user_pass) {
+    createUser().then(() => {
+        localStorage.setItem("user_name", user_name);
+        localStorage.setItem("user_pass", user_pass);
+    }).catch(err => console.log("Error creating/logging in user!", err));
+}
+function getHttp(path, headers) {
+    console.log("GETTING!", path, headers);
+    return new Promise(async (resolve, reject) => {
+        if (!headers) {
+            headers = {}
+        }
+        headers.Accept = 'application/json';
+        headers['Content-Type'] = 'application/json';
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers.user_name = user_name;
+        headers.user_pass = user_pass;
+        const response = await fetch(host + path, {
+            method: 'GET',
+            headers: headers,
+        });
+
+        response.json().then(data => {
+            console.log("GOT JSON: ", data);
+            resolve(data);
+        }).catch(error => {
+            console.log("NO JSON :(")
+            resolve({})
+            // reject(error);
+        })
+    })
+}
+
+function postHttp(path, body, headers) {
+    console.log("POSTING!", path, body, headers);
+    return new Promise(async (resolve, reject) => {
+        if (!headers) {
+            headers = {}
+        }
+        headers.Accept = 'application/json';
+        headers['Content-Type'] = 'application/json';
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers.user_name = user_name;
+        headers.user_pass = user_pass;
+        
+        const response = await fetch(host + path, {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body),
+        });
+        response.json().then(data => {
+            console.log("GOT JSON: ", data);
+            resolve(data);
+        }).catch(error => {
+            console.log("NO JSON :(");
+            resolve({});
+            // reject(error);
+        })
+    })
+}
+
+function hashPassword(pass) {
+    return pass;
+}
+function createUser() {
+    return new Promise(async (resolve, reject) => {
+        res = await postHttp("members/add", {});
+        if (res.group_id) {
+            resolve(res.group_id);
+        } else {
+            reject(res);
+        }
+    });   
+}
+
+function joinGroup(group_id) {
+    return new Promise(async (resolve, reject) => {
+        res = await postHttp("members/joingroup", {
+            group_id: group_id,
+        });
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getGroupIOwn() {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("members/group");
+        if (res.group_id) {
+            resolve(res.group_id)
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getGroupIBelongTo() {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("members/allgroups")
+        if (res.group_id) {
+            resolve(res.group_id);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getMyPoints() {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("currency/user");
+        if (res.points) {
+            resolve(res.points);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function setUserPoints(user_name, points) {
+    return new Promise(async (resolve, reject) => {
+        res = await postHttp("currency/set", {
+            user_name: user_name,
+            points: points,
+        });
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function redeemReward(taskName) {
+    return new Promise(async (resolve, reject) => {
+        res = await postHttp("task/submit", {
+            reward_name: taskName,
+        });
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function addTask(taskName, description, time, points, repeat) {
+    return new Promise(async (resolve, reject) => {
+        let postBody = {
+            task_name: taskName,
+            description: description,
+            type: "one off",
+            time: time,
+            points: points,
+        }
+        if (repeat != null) {
+            postBody.type = "repeat";
+            postBody.repeat = repeat;
+        }
+        res = await postHttp("task/add",postBody);
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getUserSubmittedRewards() {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getUserSubmittedRewards");
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function reviewUserSubmittedTask(review, task_id) {
+    return new Promise(async (resolve, reject) => {
+        res = await postHttp("reviewUserSubmittedTask", {
+            review: review,
+        }, {task_id: task_id});
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getTaskFromGroupIOwn(task) {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getTaskFromGroupIOwn", {task_name: task});
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getTaskFromGroupIBelong(task) {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getTaskFromGroupIBelong", {task_name: task});
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getTasksFromGroupIOwn() {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getTasksFromGroupIOwn");
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getTasksFromGroupIBelong() {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getTasksFromGroupIBelong");
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function addRewards(rewardName, description, points) {
+    return new Promise(async (resolve, reject) => {
+        res = await postHttp("addReward", {
+            reward_name: rewardName,
+            description: description,
+            points: points,
+        });
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getUserSubmittedTasks() {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getUserSubmittedTasks");
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function reviewUserSubmittedReward(review, task_id) {
+    return new Promise(async (resolve, reject) => {
+        res = await postHttp("reviewUserSubmittedReward", {
+            review: review,
+        }, {task_id: task_id});
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getRewardFromGroupIOwn(reward_id) {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getRewardFromGroupIOwn", {reward_id: reward_id});
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getRewardFromGroupIBelong(reward_id) {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getRewardFromGroupIBelong", {reward_id: reward_id});
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getRewardsFromGroupIOwn() {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getRewardsFromGroupIOwn");
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
+
+function getRewardsFromGroupIBelong() {
+    return new Promise(async (resolve, reject) => {
+        res = await getHttp("getRewardsFromGroupIBelong");
+        if (!res.error) {
+            resolve(res);
+        } else {
+            reject(res);
+        }
+    })
+}
---
+++ website/pageScript.js
@@ -0,0 +1,83 @@
+prev = window.onload();
+window.onload = () => {
+    prev && prev();
+    if(isLoggedIn()) {
+        checkGroupMembership();
+    }
+}
+
+function checkGroupMembership() {
+    getGroupIBelongTo().then(group_id => {
+        switchToUserScreen();
+    }).catch(err => {
+        reset_swaps();
+        swap("loginScreen", "groupJoinScreen");
+    });
+}
+
+function logInSignUp(type) {
+    const usernameElm = document.getElementById("username");
+    if(!checkWarnTextVal(usernameElm)) return;
+    const passwordElm = document.getElementById("password");
+    if(!checkWarnTextVal(passwordElm)) return;
+    
+    const username = usernameElm.value;
+    const password = passwordElm.value;
+    
+    login(username, password);
+}
+
+function joinGroupButton() {
+    const groupElm = document.getElementById("groupID");
+    if(!checkWarnTextVal(groupElm)) return;
+    const group_id = groupElm.value;
+    joinGroup(group_id).then(() => {
+        reset_swaps();
+        checkGroupMembership();
+    });
+}
+
+function createGroupButton() {
+    const groupElm = document.getElementById("groupID");
+    if(!checkWarnTextVal(groupElm)) return;
+    const group_id = groupElm.value;
+    // TODO: change your group id to this one
+    reset_swaps();
+    // checkGroupMembership();
+}
+
+function createTaskElement(task) {
+    const elm = document.createElement("div");
+    // add task properties to div
+    return elm;
+}
+function createRewardElement(reward) {
+    const elm = document.createElement("div");
+    // add task properties to div
+    return elm;
+}
+
+async function switchToUserScreen() {
+    reset_swaps();
+    swap("login_gui", "user_panel");
+    const myPoints  = await getMyPoints();
+    const myTasks   = await getTasksFromGroupIBelong();
+    const myRewards = await getRewardsFromGroupIBelong();
+    
+    const userInfoTextElm = document.getElementById("userInfoText");
+    userInfoTextElm.innerHTML = `${username} - ${myPoints} points`;
+    
+    const tasks = getTaskFromGroupIBelong(task);
+    const rewards = getRewardsFromGroupIBelong(task);
+    const task_container = document.getElementById("task_container");
+    const reward_container = document.getElementById("reward_container");
+    
+    task_container.children = []
+    reward_container.children = []
+    for(const task of tasks) {
+        task_container.appendChild(createTaskElement(task));
+    }
+    for(const reward of rewards) {
+        task_container.appendChild(createRewardElement(reward));
+    }
+}
