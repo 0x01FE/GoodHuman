26d1792d0b541b85351631b706c968f8c1dd1155
GanerCodes <ganer@ganer.xyz>
2022-11-12 15:17:07 -0600

    xd

--- app/__init__.py
+++ app/__init__.py
@@ -24,14 +24,15 @@ class Opener():
         self.con = sql.connect(DATABASE)
         self.req = req
     def __enter__(self):
-        return con, con.cursor(), try_parse_json(self.req), self.req.headers
+        return self.con, self.con.cursor(), try_parse_json(self.req), self.req.headers
     def __exit__(self, type, value, traceback):
+        self.con.commit()
         self.con.close()
 
 @app.route('/members/add', methods=['POST'])
 def createUser():
-    with Opener(request) as con, cur, data, headers:
-        cur.execute("SELECT * FROM members WHERE user_name = ?", headers['user-name'])
+    with Opener(request) as (con, cur, data, headers):
+        cur.execute("SELECT * FROM members WHERE user_name = ?", [headers['user-name']])
         db_data = cur.fetchall()
         if not db_data:
             return '', 204
@@ -41,32 +42,24 @@ def createUser():
             else:
                 group = str(random())
             cur.execute("INSERT INTO members VALUES (?, ?, ?, NULL)", [headers['user-name'], headers['user_pass'], group])
-            con.commit()
             return '', 204
 
 @app.route('/members/group', methods=['GET'])
 def getGroupIOwn():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = try_parse_json(request)
-        headers = request.headers
+    with Opener(request) as (con, cur, data, headers):
         cur.execute("SELECT group_id FROM members WHERE user_name = ?", [headers['user-name']])
         db_data = cur.fetchone()
         db_data = db_data.split(",")
         response = app.response_class(
             response=json.dumps({"group_id":str(db_data[0])}),
             status=200,
-            mimetype='application/json'
-        )
+            mimetype='application/json')
         response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
 @app.route('/members/allgroups', methods=['GET'])
 def getMyGroups():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = try_parse_json(request)
-        headers = request.headers
+    with Opener(request) as (con, cur, data, headers):
         cur.execute("SELECT group_id FROM members WHERE user_name = ?", [headers['user-name']])
         db_data = cur.fetchone()
         db_data = db_data.split(',')
@@ -74,81 +67,53 @@ def getMyGroups():
         response = app.response_class(
             response=json.dumps({"group_id":db_data}),
             status=200,
-            mimetype='application/json'
-        )
+            mimetype='application/json')
         response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
 @app.route('/members/joingroup', methods=['POST'])
 def joinGroup():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = try_parse_json(request)
-        headers = request.headers
+    with Opener(request) as (con, cur, data, headers):
         db_data = cur.fetchone()
         db_data = db_data + f",{str(data['group_id'])}"
         cur.execute("UPDATE members SET group_id = ? WHERE user_name = ?", [db_data, headers['user-name']])
-        con.commit()
     return '', 204
  
-
-
-# CURRENCY
-
 @app.route('/currency/user', methods=['GET'])
 def getUserCurrency():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = try_parse_json(request)
-        headers = request.headers
+    with Opener(request) as (con, cur, data, headers):
         cur.execute("SELECT currency_ammount FROM members WHERE user_name = ?", [headers['user-name']])
         currency = cur.fetchone()
         response = app.response_class(
             response=json.dumps({'points':currency}),
             status=200,
-            mimetype='application/json'
-        )
+            mimetype='application/json')
         response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
 @app.route('/currency/set', methods=['POST'])
 def setUserCurrency():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = try_parse_json(request)
-        headers = request.headers
+    with Opener(request) as (con, cur, data, headers):
         cur.execute("UPDATE members SET currency_ammount = ? WHERE user_name = ?", [data['points'], data['user_name']])
-        con.commit()
     return '', 204
 
 # this is for local use (not an endpoint)
 def addUserCurrency(user_name,ammount):
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
+    with Opener(request) as (con, cur, data, headers):
         cur.execute("SELECT currency_ammount FROM members WHERE user_name = ?", [user_name])
         new_points = int(cur.fetchone()) + ammount
         cur.execute("UPDATE members SET currency_ammount = ? WHERE user_name = ?", [new_points, user_name])
-        con.commit()
-
-# TASKS
 
 @app.route('/task/submit', methods=['POST'])
 def submitTask():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = try_parse_json(request)
-        headers = request.headers
+    with Opener(request) as (con, cur, data, headers):
         time_now = datetime.now().strftime("%H:%M")
         cur.execute("UPDATE tasks SET image = ?, user_name = ?, pending = 1, submit_time = ? WHERE task_name = ?", [data['image'], headers['user-name'], time_now, data['task-name']])
-        con.commit()
     return '', 204
 
 @app.route('/task/add', methods=['POST'])
 def addTask():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = try_parse_json(request)
-        headers = request.headers
+    with Opener(request) as (con, cur, data, headers):
         cur.execute("SELECT task_id FROM tasks ORDER BY task_id DESC")
         highest_id = cur.fetchone()
         if not highest_id:
@@ -156,8 +121,34 @@ def addTask():
         else:    
             highest_id+=1
         cur.execute("INSERT INTO tasks VALUES (?, ?, NULL, ?, ?, ?, ?, ?, NULL, NULL, NULL)", [highest_id+1, data['task-name'], data['points'], data['type'], data['repeat'], data['time'], data['description']])
-        con.commit()
     return '', 204
+
+def getTask(task_id):
+    with Opener(request) as (con, cur, data, headers):
+        cur.execute("SELECT * FROM tasks WHERE task_id = ?", [task_id])
+        task = cur.fetchone()
+    return task
+
+@app.route('/reward/redeem', methods=['POST'])
+def redeemReward():
+    with Opener(request) as (con, cur, data, headers):
+        time_now = datetime.now().strftime("%H:%M")
+        cur.execute(f"UPDATE rewards SET user_name = ?, pending = 1, submit_time = ? WHERE reward_name ?", [headers['user-name'], time_now, data['reward_name']])
+    return '', 204
+
+@app.route('/reward/pending', methods=['GET'])
+def getPendingRewards():
+    with Opener(request) as (con, cur, data, headers):
+        cur.execute("SELECT user_name, reward_id, reward_name, submit_time FROM rewards WHERE pending = 1")
+        pending_rewards = cur.fetchall()
+        response = app.response_class(
+            response=json.dumps({"pending_rewards":pending_rewards}),
+            status=200,
+            mimetype='application/json')
+        response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
 '''
 @app.route("/task/review", methods=['POST'])
 def reviewTask():
@@ -179,57 +170,3 @@ def reviewTask():
             case 'deny':
     return '', 204
 '''
-def getTask(task_id):
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM tasks WHERE task_id = ?", [task_id])
-        task = cur.fetchone()
-        print(task)
-        return task
-
-
-
-
-
-
-# REWARDS
-
-@app.route('/reward/redeem', methods=['POST'])
-def redeemReward():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = try_parse_json(request)
-        headers = request.headers
-        time_now = datetime.now().strftime("%H:%M")
-        cur.execute(f"UPDATE rewards SET user_name = ?, pending = 1, submit_time = ? WHERE reward_name ?", [headers['user-name'], time_now, data['reward_name']])
-        con.commit()
-    return '', 204
-
-
-@app.route('/reward/pending', methods=['GET'])
-def getPendingRewards():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        data = try_parse_json(request)
-        headers = request.headers
-        cur.execute("SELECT user_name, reward_id, reward_name, submit_time FROM rewards WHERE pending = 1")
-        pending_rewards = cur.fetchall()
-        response = app.response_class(
-            response=json.dumps({"pending_rewards":pending_rewards}),
-            status=200,
-            mimetype='application/json'
-        )
-        response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
-
-
-
-        
-
-
-
-
-
-
-
-
